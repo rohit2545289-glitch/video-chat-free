001ac627e167d3c771f53bab741244f7e09cece0
rohit2545289-glitch <rohit2545289@gmail.com>
2026-07-04 23:21:35 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,11 @@
+// config.js - Firebase Configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyBVN3GIrV_dw6J3ytSU9upkI8ciTsD9yOc",
+    authDomain: "db101-ff3d6.firebaseapp.com",
+    databaseURL: "https://db101-ff3d6-default-rtdb.firebaseio.com",
+    projectId: "db101-ff3d6",
+    storageBucket: "db101-ff3d6.firebasestorage.app",
+    messagingSenderId: "447900692217",
+    appId: "1:447900692217:web:6aa6ea9806ca0a9a3abc28"
+};
+
